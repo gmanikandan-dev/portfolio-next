742c2875c47d6b601809d0124000e04873199445
Manikandan G <gmanikandan845@gmail.com>
2025-07-20 22:06:25 +0530

    fix: build issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center h-screen p-4 bg-amber-300">
-      <h1 className="text-4xl font-bold">I'm a Full Stack Developer</h1>
+      <h1 className="text-4xl font-bold">I&apos;m a Full Stack Developer</h1>
       <p className="text-lg">
-        I'm a full stack developer with a passion for building web applications.
+        I&apos;m a full stack developer with a passion for building web applications.
       </p>
       </div>
     </div>
